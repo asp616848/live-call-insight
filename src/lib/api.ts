8067081219518666fd30044d0a57fa1c72928729
Abhijeet Shashwat <asp616848@gmail.com>
2025-08-21 14:30:31 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -24,7 +24,7 @@ export const API_BASE = (() => {
   }
 
   // Default for local dev
-  return 'http://15.206.28.101:5000';
+  return 'http://15.206.28.101:5001';
 })();
 
 export function apiUrl(path: string) {
